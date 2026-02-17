026894f5e04cc81d862a3fcb15ad0f0c240104b2
Raúl <raul.parada040103@gmail.com>
2026-02-17 02:19:37 +0100

    deps: Instaladas dependencias sitemap y lucide + config robots y sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://devparada.dev',
+  integrations: [sitemap()],
   vite: {
+    // @ts-ignore - Conflicto de tipos entre Vite 6 y 7
     plugins: [tailwindcss()]
   }
 });
