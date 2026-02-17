7270a0919ec4db5c459f852ba410e2721273f09a
Raúl <raul.parada040103@gmail.com>
2026-02-18 00:18:51 +0100

    Uso de constantes para las redes sociales y el correo electrónico

---
+++ src/constants/contants.ts
@@ -0,0 +1,8 @@
+// Este archivo contiene constantes como los enlaces a redes sociales y el correo electrónico
+export const SITE_DATA = {
+  email: "raul.paradadelafuente@gmail.com",
+  socials: {
+    github: "https://github.com/devparada/",
+    linkedin: "https://www.linkedin.com/in/raul-parada-de-la-fuente/",
+  },
+};
