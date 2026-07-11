dd930b3b8c7c61a8daa100b1a765567d46fe10d1
Raúl <raul.parada040103@gmail.com>
2026-07-11 11:20:09 +0200

    feat: Portfolio renovado

--- src/constants/contants.ts
+++ src/constants/contants.ts
@@ -1,8 +1,85 @@
 // Este archivo contiene constantes como los enlaces a redes sociales y el correo electrónico
-export const SITE_DATA = {
-  email: "raul.paradadelafuente@gmail.com",
-  socials: {
-    github: "https://github.com/devparada/",
-    linkedin: "https://www.linkedin.com/in/raul-parada-de-la-fuente/",
+
+export const PROYECTOS = [
+  {
+    nombre: "RitmosBot",
+    urlCodigo: "https://github.com/devparada/RitmosBot",
+    urlDemo: "https://ritmosweb.vercel.app",
+    urlImg: "/src/assets/ritmos-proyecto.png",
+    descripcion:
+      "Bot de música que permite a los usuarios disfrutar de su música en Discord con comandos simples.",
+    lenguajes: [
+      { nombre: "NodeJS" },
+      { nombre: "JavaScript" },
+      { nombre: "TypeScript" },
+      { nombre: "Jest" },
+      { nombre: "Discord.js" },
+    ],
   },
+  {
+    nombre: "TresEnrayaJS",
+    urlCodigo: "https://github.com/devparada/TresEnRayaJS",
+    urlDemo: "https://tresenraya.devparada.dev",
+    urlImg: "/src/assets/tresenrayajs-proyecto.png",
+    descripcion:
+      "Versión del tres en raya en la que puedes jugar contra la CPU o contra otro jugador en la misma pantalla.",
+    lenguajes: [
+      { nombre: "HTML5" },
+      { nombre: "CSS3" },
+      { nombre: "JavaScript" },
+    ],
+  },
+  {
+    nombre: "TaskVelocity",
+    urlCodigo: "https://github.com/devparada/taskvelocity",
+    urlImg: "/src/assets/taskvelocity-proyecto.png",
+    descripcion:
+      "Proyecto de fin de ciclo de desarrollo de aplicaciones web, que permite a los usuarios gestionar sus tareas de manera eficiente y visual.",
+    lenguajes: [
+      { nombre: "PHP" },
+      { nombre: "MySQL" },
+      { nombre: "Bootstrap" },
+    ],
+  },
+  {
+    nombre: "Proyecto Pendiente",
+    urlCodigo: "https://github.com/devparada",
+    urlDemo: "https://devparada.dev",
+    urlImg: "/src/assets/ritmos-proyecto.png",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vivamus elementum semper nisi.",
+    lenguajes: [
+      { nombre: "React" },
+      { nombre: "Three JS" },
+      { nombre: "Tailwind CSS" },
+    ],
+  },
+];
+
+export const CERTIFICACIONES = [
+  {
+    nombre: "Drupal 11 Backend",
+    empresa: "Forcontu",
+    fecha: "Abril 2026",
+    url: "#",
+  },
+  {
+    nombre: "FCOI12 - Análisis de Información Digital",
+    empresa: "Junta de Galicia",
+    fecha: "Enero 2026",
+    url: "#",
+  },
+];
+
+export const REDES_SOCIALES = {
+  email: "raul.paradadelafuente@gmail.com",
+  github: "https://github.com/devparada/",
+  linkedin: "https://www.linkedin.com/in/raul-parada-de-la-fuente/",
+  discord: "https://discord.com/channels/@me/554718560916733955"
+};
+
+export const STACK_TECNOLOGICO = {
+  frontend: ["JavaScript", "TypeScript", "React", "Astro", "Tailwind CSS"],
+  backend: ["Node.js", "Java", "MySQL"],
+  herramientas: ["Git", "Docker", "Figma"],
 };
